83363e7d558456a02f7417fbc4a4e2e27af0f43d
@Kavita2599 <kavita99@netart.io>
2025-02-20 14:00:47 +0530

    connected contact form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { useForm, ValidationError } from '@formspree/react';
+
 function ContactForm() {
-    // const [state, handleSubmit] = useForm("myzgvndp");
+    const [state, handleSubmit] = useForm("xyzkogzl");
     const InputStyle = {
-        width: "90%", background: "transparent", border: "1px solid #575757", padding: "14px", borderRadius: "25px", fontSize: "18px", lineHeight: "25px"
+        width: "90%",color: "#ffffff", background: "transparent", border: "1px solid #575757", padding: "14px", borderRadius: "25px", fontSize: "18px", lineHeight: "25px"
     }
-    const labelStyle = { color: "#FCFCFC", fontSize: "16px", lineHeight: "0px", margin: "8px 0 0 0" }
+    const labelStyle = { color: "#ffffff", fontSize: "16px", lineHeight: "0px", margin: "8px 0 0 0" }
     return (
-        <>
-            <form style={{ display: "flex", flexDirection: "column", gap: "24px", width: "100%", flex: 1 }}>
+        <>   {state.succeeded ? <p style={{ textAlign: 'center', marginTop: '20px' }}>Thanks! We will get back to you shortly</p> :
+            <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: "24px", width: "100%", flex: 1 }}>
                 <p style={labelStyle}>Your Name</p>
                 <input style={InputStyle} type='text' name='name' placeholder='Name' required></input>
                 <p style={labelStyle}>Your Email</p>
@@ -16,8 +18,9 @@ function ContactForm() {
                 <input style={InputStyle} type='text' name='subject' placeholder='Subject' required></input>
                 <p style={labelStyle}>Message</p>
                 <textarea style={{ height: "120px", flex: 1, resize: "none", ...InputStyle }} type='text' name='message' placeholder='Message' ></textarea>
-                <button type='submit' style={{ padding: "14px 28px", background: "#FDC500", border: "none", borderRadius: "25px", fontWeight: 'bold', width: "max-content" }}>Get Started</button>
+                <button type='submit' disabled={state.submitting} style={{ padding: "14px 28px", background: "#FDC500", border: "none", borderRadius: "25px", fontWeight: 'bold', width: "max-content" }}>Get Started</button>
             </form>
+        }
         </>
     )
 }
